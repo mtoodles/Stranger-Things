fd7880119ad3c736051a9ec59350014e5e006f8a
garrettbolen <59625632+garrettbolen@users.noreply.github.com>
2020-05-20 00:43:35 -0700

    Added new level and game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class DeathZone : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
+    public void LoadNextLevel(int x)
+    {
+        SceneManager.LoadScene(x);
+    }
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevel : MonoBehaviour
+{
+    GameManager gm;
+    public int levelNum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = FindObjectOfType<GameManager>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        gm.LoadNextLevel(levelNum);
+    }
+}
