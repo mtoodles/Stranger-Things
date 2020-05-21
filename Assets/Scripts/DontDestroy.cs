987c00bd639c0ce5383a55f6f8dc12085f3142d7
garrettbolen <59625632+garrettbolen@users.noreply.github.com>
2020-05-20 19:22:59 -0700

    Fixed transition between levels

--- Assets/Scripts/DontDestroy.cs
+++ Assets/Scripts/DontDestroy.cs
@@ -1,20 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class DontDestroy : MonoBehaviour
 {
+    private bool objectExists = false;
     void Awake()
     {
-        GameObject[] objs = GameObject.FindGameObjectsWithTag("music");
-        if (objs.Length > 1)
+        if (!objectExists)
+        {
+            objectExists = true;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
             Destroy(this.gameObject);
-        GameObject[] objs2 = GameObject.FindGameObjectsWithTag("UI");
-        if (objs.Length > 1)
-            Destroy(this.gameObject);
-        GameObject[] objs3 = GameObject.FindGameObjectsWithTag("Player");
-        if (objs.Length > 1)
-            Destroy(this.gameObject);
-        DontDestroyOnLoad(this.gameObject);
+        }
     }
 }
+//Alternate code
+//GameObject[] objs = GameObject.FindGameObjectsWithTag("music");
+//        if (objs.Length > 1)
+//            Destroy(this.gameObject);
+//GameObject[] objs2 = GameObject.FindGameObjectsWithTag("UI");
+//        if (objs.Length > 1)
+//            Destroy(this.gameObject);
+//GameObject[] objs3 = GameObject.FindGameObjectsWithTag("Player");
+//        if (objs.Length > 1)
+//            Destroy(this.gameObject);
+//DontDestroyOnLoad(this.gameObject);
---
+++ Assets/Scripts/PlayerStartPoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PlayerStartPoint : MonoBehaviour
+{
+    private PlayerMovement player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>();
+        player.transform.position = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
